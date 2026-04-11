bafbbb1152582e9406ef648a953d9654ee64c03a
satyamfied <satyamfied18@gmail.com>
2026-04-11 18:35:33 +0530

    Updated OpenAI API with free API of OpenRouter and fixed package.json warning for type: module

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,16 @@
-import OpenAI from 'openai';
-import 'dotenv/config';
+import OpenAI from "openai";
+import "dotenv/config";
 
 const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, 
+  apiKey: process.env.OPENROUTER_API_KEY,
+
+  baseURL: "https://openrouter.ai/api/v1",
 });
 
 const response = await client.responses.create({
-  model: 'gpt-4o-mini',
-  input: 'Tell a joke about computer science',
+  model: "openai/gpt-4o-mini",
+
+  input: "Tell a joke about computer science",
 });
 
 console.log(response.output_text);
