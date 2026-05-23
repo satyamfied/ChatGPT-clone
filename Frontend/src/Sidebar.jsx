5ad64356cb4ff6e05f54419de161c073bc1c264d
satyamfied <satyamfied18@gmail.com>
2026-05-23 19:18:47 +0530

    Built the frontend Blueprint and the sidebar

---
+++ Frontend/src/Sidebar.jsx
@@ -0,0 +1,27 @@
+import "./Sidebar.css";
+
+function Sidebar() {
+    return (
+        <section className="sidebar">
+            {/* New Chat Button */}
+            <button>
+                <img src="src/assets/blacklogo.png" alt="GPT logo" className="logo"></img>
+                <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+
+            {/* History */}
+            <ul className="history">
+                <li>Thread1</li>
+                <li>Thread2</li>
+                <li>Thread3</li>
+            </ul>
+
+            {/* Sign */}
+            <div className ="sign">
+                <p>Made by Satyam Kumar</p>
+            </div>
+        </section>
+    )
+}
+
+export default Sidebar;
