7094950294dff21596c82c669f3c253f5af93061
satyamfied <satyamfied18@gmail.com>
2026-05-24 15:46:29 +0530

    Styling on ChatWindow and MessageBox

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,9 +1,29 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 
 function ChatWindow() {
     return (
-        <div>
-            ChatWindow
+        <div className="chatWindow">
+            <div className="navbar">
+                <span>ChatGPT <i class="fa-solid fa-angle-down"></i></span>
+                <div className="userIconDiv">
+                    <span className="userIcon"><i class="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+
+            <Chat></Chat>
+
+            <div className="chatInput">
+                <div className="inputBox">
+                    <input placeholder="Ask anything">
+
+                    </input>
+                    <div id="submit"> <i class="fa-solid fa-arrow-up"></i> </div>
+                </div>
+                <p className="info"> 
+                    ChatGPT can make mistakes. Check important info.
+                </p>
+            </div>
         </div>
     )
 }
